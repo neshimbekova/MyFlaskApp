8e280fbeacbb5c34e9380997f7abf3d2667cf1fc
neshimbekova <neshimbekova@gmail.com>
2019-03-27 13:13:33 -0500

    dashboard edit

--- app.py
+++ app.py
@@ -12,16 +12,24 @@ from flask_login import LoginManager, UserMixin, login_user, login_required, log
 
 app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@host/database'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://noor:Nurzhamal@165.227.110.208/noor'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://noor:0wjI7VALLnk3Zlr4@165.227.110.208/noor'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://flask:Redhat2019**@165.227.110.208/flask'
 app.config['SECRET_KEY'] = 'secret_littlesnowflake<<>>kuku'
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
@@ -68,20 +76,36 @@ def signup():
         new_user = User(username=form.username.data, email=form.email.data, password=form.password.data)
         db.session.add(new_user)
         db.session.commit()
-        return '<h1> New User Has been created! </h1>'
+        return '<h1>New user has been created!</h1>'
+        #return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
     return render_template('signup.html', form=form)
 
-@app.route('/login')
+
+#Login_Page
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if check_password_hash(user.password, form.password.data):
+                return redirect(url_for('dashboard'))
+        return '<h1>Invalid username or password</h1>'
+        #return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
     return render_template('login.html', form=form)
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html', name=current_user.username)
 
 #Logout_Page
 @app.route('/logout', methods=['GET', 'POST'])
 @login_required
 def logout():
     logout_user()
-    return render_template('home.html')
+    return redirect(url_for('home'))
+    #return render_template('login.html')
 
 
 
